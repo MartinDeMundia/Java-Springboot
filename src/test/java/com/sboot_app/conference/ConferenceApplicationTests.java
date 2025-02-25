1240c0be2610c1df92b2a4f0fcb22c52cfedbbbb
MartinDeMundia <mugambi.m.mundia@gmail.com>
2025-02-25 03:21:02 +0300

    feat: conference spring app

---
+++ src/test/java/com/sboot_app/conference/ConferenceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sboot_app.conference;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConferenceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
