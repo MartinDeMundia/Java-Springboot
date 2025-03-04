92c386b21469abd309991b9bce676d72e74a69be
MartinDeMundia <mugambi.m.mundia@gmail.com>
2025-03-04 22:33:42 +0300

    fix : View resolvers , Static resources

---
+++ src/main/java/com/sboot_app/conference/model/Registration.java
@@ -0,0 +1,13 @@
+package com.sboot_app.conference.model;
+
+public class Registration {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
